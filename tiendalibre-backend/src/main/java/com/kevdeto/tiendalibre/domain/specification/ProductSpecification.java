bdac94a0aceeb7447d7c389ba7b1271ea6315268
KevDeto <contactohuayta@gmail.com>
2025-07-18 12:00:20 -0300

    termine las specification de todas las entidades

--- tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/domain/specification/ProductSpecification.java
+++ tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/domain/specification/ProductSpecification.java
@@ -2,10 +2,7 @@ package com.kevdeto.tiendalibre.domain.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 
-import com.kevdeto.tiendalibre.domain.entity.CategoryEntity;
 import com.kevdeto.tiendalibre.domain.entity.ProductEntity;
-
-import jakarta.persistence.criteria.Join;
 
 public class ProductSpecification {
 	public static Specification<ProductEntity> hasName(String name) {
@@ -18,8 +15,8 @@ public class ProductSpecification {
 				: cb.like(cb.lower(root.get("brand")), "%" + brand.toLowerCase() + "%");
 	}
 
-	public static Specification<ProductEntity> hasIsActive(Boolean isActive) {
-		return (root, query, cb) -> isActive == null ? null 
-				: cb.equal(root.get("isActive"), isActive);
+	public static Specification<ProductEntity> hasActive(Boolean active) {
+		return (root, query, cb) -> active == null ? null 
+				: cb.equal(root.get("active"), active);
 	}
 }
