575eeac5370595ec1eca7fcad82a99909f00728b
KevDeto <contactohuayta@gmail.com>
2025-07-18 11:59:49 -0300

    agregue la logica de negocio (application/adapter) implementando los useCase

--- tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/application/adapter/ProductServiceImpl.java
+++ tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/application/adapter/ProductServiceImpl.java
@@ -1,5 +1,99 @@
 package com.kevdeto.tiendalibre.application.adapter;
 
-public class ProductServiceImpl {
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.kevdeto.tiendalibre.application.usecase.ProductUseCase;
+import com.kevdeto.tiendalibre.domain.dto.request.ProductRequestDTO;
+import com.kevdeto.tiendalibre.domain.dto.response.ProductDetailResponseDTO;
+import com.kevdeto.tiendalibre.domain.dto.response.ProductSummaryResponseDTO;
+import com.kevdeto.tiendalibre.domain.entity.ProductEntity;
+import com.kevdeto.tiendalibre.domain.mapper.ProductMapper;
+import com.kevdeto.tiendalibre.domain.repository.ProductRepository;
+import com.kevdeto.tiendalibre.domain.specification.ProductSpecification;
+import com.kevdeto.tiendalibre.shared.exception.BusinessException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProductServiceImpl implements ProductUseCase {
+
+	private final ProductRepository productRepository;
+	private final ProductMapper productMapper;
+
+	public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper) {
+		this.productRepository = productRepository;
+		this.productMapper = productMapper;
+	}
+
+	@Override
+	public List<ProductSummaryResponseDTO> getAll(Integer page, Integer size) {
+		if(page == null && size == null) {
+			throw new BusinessException("Debe proporcionar un page y un size.");
+		}
+		
+		Page<ProductEntity> productPages = productRepository.findAll(PageRequest.of(page, size));
+		List<ProductSummaryResponseDTO> listProductResponse = productPages.getContent().stream()
+				.map(productMapper::toSummaryResponse).toList();
+		return listProductResponse;
+	}
+
+	@Override
+	public List<ProductSummaryResponseDTO> search(String name, String brand, Boolean active, Integer size) {
+		if (name == null && brand == null && active == null && size == null) {
+			throw new BusinessException("Debe proporcionar al menos un filtro de busqueda.");
+		}
+				
+		Specification<ProductEntity> productSpec = ProductSpecification.hasName(name)
+				.and(ProductSpecification.hasBrand(brand))
+				.and(ProductSpecification.hasActive(active));
+		
+		List<ProductSummaryResponseDTO> listProductSummaryResponse = productRepository
+				.findAll(productSpec, PageRequest.of(0, size))
+				.getContent()
+				.stream()
+				.map(productMapper::toSummaryResponse)
+				.toList();
+		
+		return listProductSummaryResponse;
+	}
+
+	@Override
+	public ProductDetailResponseDTO getById(Long id) {
+		ProductEntity product = productRepository.findById(id)
+				.orElseThrow(()-> new EntityNotFoundException("Producto no encontrado"));
+		ProductDetailResponseDTO productDetailResponse = productMapper.toDetailResponse(product);
+		return productDetailResponse;
+	}
+
+	@Override
+	public ProductDetailResponseDTO create(ProductRequestDTO productRequest) {
+		ProductEntity product = productMapper.toEntity(productRequest);
+		ProductEntity productSave = productRepository.save(product);
+		ProductDetailResponseDTO productDetailReponse = productMapper.toDetailResponse(productSave);
+		return productDetailReponse;
+	}
+
+	@Override
+	public ProductDetailResponseDTO update(Long id, ProductRequestDTO requestDTO) {
+		ProductEntity product = productRepository.findById(id)
+				.orElseThrow(()->new EntityNotFoundException("Producto con ID "+id+" no encontrado."));
+		
+		productMapper.updateEntityFromDTO(requestDTO, product);
+		ProductEntity upgradedProduct = productRepository.save(product);
+		ProductDetailResponseDTO productDetailReponse = productMapper.toDetailResponse(upgradedProduct);
+		return productDetailReponse;
+	}
+
+	@Override
+	public void delete(Long id) {
+		if(!productRepository.existsById(id)) {
+			throw new EntityNotFoundException("Producto con ID "+id+" no encontrado.");
+		}
+		productRepository.deleteById(id);
+	}
 }
--- tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/application/usecase/ProductUseCase.java
+++ tiendalibre-backend/src/main/java/com/kevdeto/tiendalibre/application/usecase/ProductUseCase.java
@@ -1,5 +1,16 @@
 package com.kevdeto.tiendalibre.application.usecase;
 
-public interface ProductUseCase {
+import java.util.List;
 
+import com.kevdeto.tiendalibre.domain.dto.request.ProductRequestDTO;
+import com.kevdeto.tiendalibre.domain.dto.response.ProductDetailResponseDTO;
+import com.kevdeto.tiendalibre.domain.dto.response.ProductSummaryResponseDTO;
+
+public interface ProductUseCase {
+	List<ProductSummaryResponseDTO> getAll(Integer page, Integer size);
+	List<ProductSummaryResponseDTO> search(String name, String brand, Boolean active, Integer size);
+	ProductDetailResponseDTO getById(Long id);
+	ProductDetailResponseDTO create(ProductRequestDTO productRequest);
+	ProductDetailResponseDTO update(Long id, ProductRequestDTO requestDTO);
+	void delete(Long id);
 }
